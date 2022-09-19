5624c97b21e665977f993fe20943221a626b3991
Vito <vito_bjj@mail.ru>
2022-09-19 19:15:51 +0300

    Will create the initial version of the program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        
+
         double myBmi = service.calculate(80, 1.8); // Рост в метрах, вес в килограммах
         System.out.println(myBmi);
     }
